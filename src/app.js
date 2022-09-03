1f481bbb8ce31bcbd8dcfacf8e45f4bc60db986d
Negar Sourati <negarsourati@Negars-MacBook-Pro.local>
2022-09-03 21:41:02 +0430

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const forecast = require('./utils/forecast')
 // console.log(path.join())
 
 const app = express();
+const port = process.env.PORT || 3000;
 //define paths for express configuration
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -109,6 +110,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('listening on port 3000')
+app.listen(port, () => {
+    console.log('listening on port ' + port)
 })
