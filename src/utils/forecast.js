a27e1f01add89391414a86ca3d05383b20f07424
Negar Sourati <negarsourati@Negars-MacBook-Pro.local>
2022-09-03 23:13:23 +0430

    add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -5,12 +5,13 @@ const forecast = (latitude, longitude, callback) => {
     const url = `http://api.weatherapi.com/v1/forecast.json?key=978146f9d02e4aa7a6d120217223108&q=${latitude},${longitude}&days=1`
     request({ url, json:true }, (error, {body}) => {
         console.log(body)
+        console.log(body.humidity)
         if(error) {
             callback('connection error', undefined)
         } else if (body.error) {
             callback('unable to find location', undefined)
         } else {
-            callback(undefined, `${body.current.condition.text} ,its currently ${body.current.temp_c} degress out.`)
+            callback(undefined, `${body.current.condition.text} ,its currently ${body.current.temp_c} degress out. humidity is ${body.current.humidity}%`)
         }
     })
 }
